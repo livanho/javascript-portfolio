a588551cc04539ac24b10290ed4bf40f29f63982
Livan H <156054609+livanho@users.noreply.github.com>
2024-12-29 02:03:33 +0000

    updated package.json

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
+  name: "Livan Hagi Osman",
+  title: "Software Engineer",
+  email: "livan.hagiosman@seattlecollege.edu",
+  gitHub: "livanho",
   instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  linkedIn: "livan-hagi-osman",
+  medium: "livanhg",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
