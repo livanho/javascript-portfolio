d3a727c2ebe688b2eebd2d98f59b45879d70b2e5
Livan H <156054609+livanho@users.noreply.github.com>
2024-12-29 02:05:44 +0000

    fixed image name

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/laptop-1512838_1280";
+import image from "../images/laptop-1512838_1280.png";
 
 const imageAltText = "vector of a laptop";
 
